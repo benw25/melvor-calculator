73d892e6050b3ef278a4f0210d5bb203ede20690
Ben Wu <21284372+benw25@users.noreply.github.com>
2021-04-11 01:33:29 -0700

    babel, aorpheat

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
 
-import { logInputNumberNeeded } from './lib';
+import { calcAllAorpheats, logAllAorpheats, logInputNumberNeeded } from './lib';
 
 logInputNumberNeeded(2338, 30, 25);
+
+console.log('');
+
+logAllAorpheats(calcAllAorpheats(90, 3.75));
